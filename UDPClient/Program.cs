4b6a4fb630ac97d0164e8ec4d7ddc870d54ed7a8
Sadi_er59 <Sadi_er59@STHQ0118-08>
2021-12-23 10:53:48 +0400

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var ip = IPAddress.Loopback;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+
+
+            try
+            {
+                while (true)
+                {
+                    var text = Console.ReadLine();
+                    var bytes = Encoding.UTF8.GetBytes(text);
+                    socket.SendTo(bytes, ep);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Any;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+            socket.Bind(ep);
+
+            while (true)
+            {
+                var bytes = new byte[socket.ReceiveBufferSize];
+                EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                var length = socket.ReceiveFrom(bytes, ref endPoint);
+                var msg = Encoding.UTF8.GetString(bytes,0,length);
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
